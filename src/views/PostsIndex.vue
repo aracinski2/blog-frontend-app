f790465e99076b5cc70198484d24c9a337d840af
Alex Racinski <aracinski2@gmail.com>
2020-12-15 20:30:32 -0600

    add posts index page

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <!-- <h1>{{ posts }}</h1> -->
+    <div v-for="post in posts">
+      <p>{{ post.title }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      console.log("posts index ....");
+      axios.get("/api/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
