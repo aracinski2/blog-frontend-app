f277962332e94d37942b266f9f1bf34b71929158
Alex Racinski <aracinski2@gmail.com>
2020-12-15 18:49:18 -0600

    add test page, initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      console.log("posts index...");
+      axios;
+    },
+  },
+};
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Test page!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
