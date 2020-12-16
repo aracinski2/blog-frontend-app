e804d646f78b2a4b90bd9311e6983c9c2b2a9c17
Alex Racinski <aracinski2@gmail.com>
2020-12-15 19:28:28 -0600

    add signup

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
+import Signup from "../views/Signup.vue";
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/test',
     name: 'Test',
     component: Test
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
   }
 ];
 
