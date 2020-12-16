814fe759a2c2db41248604f400252725bc962533
Alex Racinski <aracinski2@gmail.com>
2020-12-15 21:24:06 -0600

    add show page

---
+++ src/views/PostsShow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h1>{{ post.title }}</h1>
+    <h4>{{ post.body }}</h4>
+    <img v-bind:src="post.image">
+    
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      post: [],
+    };
+  },
+  created: function () {
+    this.postsShow();
+  },
+  methods: {
+    postsShow: function () {
+      console.log("in posts show");
+      axios.get("/api/posts/4").then((response) => {
+        console.log(response.data);
+        this.post = response.data;
+      });
+    },
+  },
+};
+</script>
