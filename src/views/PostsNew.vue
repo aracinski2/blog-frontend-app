4d3a8a6eee0d3043d1d9349c6a5ce8a772763265
Alex Racinski <aracinski2@gmail.com>
2020-12-20 10:13:19 -0600

    add status error pictures

--- src/views/PostsNew.vue
+++ src/views/PostsNew.vue
@@ -3,6 +3,7 @@
 <template>
   <div class="posts-new">
     <form v-on:submit.prevent="submit()">
+      <img v-if="status" v-bind:src="`https://http.cat/${status}`" width="700px">
       <h1>New Post</h1>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
@@ -36,6 +37,7 @@ export default {
       body: "",
       image: "",
       errors: [],
+      status: "",
     };
   },
   methods: {
@@ -52,6 +54,8 @@ export default {
         })
         .catch((error) => {
           this.errors = error.response.data.errors;
+          console.log(error.response.status);
+          this.status = error.response.status;
         });
     },
   },
